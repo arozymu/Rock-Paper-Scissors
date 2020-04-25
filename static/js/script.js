100b72408611c8ae1daf3d2e377fb861fa73c91a
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2020-04-25 07:47:04 -0700

    <3>

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,24 @@
 function rpsGame(yourChoice) {
     console.log(yourChoice);
+    //var humanChoice, botChoice;
+    // humanChoice = yourChoice.id
+    botChoice = numberToChoice(randToRpsInt());
+    // alert(botChoice);
+    //results = decideWinner(humanChoice, botChoice);
+//message = finalMessage(results); // you won
+//rpsFrontEnd(yourChoice.id, botChoice, message);
 
 }
+
+function randToRpsInt () {
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return [ 'rock', 'paper', 'scissors'][number];
+}
+ function decideWinner(yourChoice, computerChoice) {
+     var rpsDatabase = {
+         
+     }
+ }
