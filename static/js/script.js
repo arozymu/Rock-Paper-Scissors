46cd4b8ea9099c79bec0c082debd6668c7cbec5c
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2020-04-30 06:01:17 -0700

    <10>

--- static/js/script.js
+++ static/js/script.js
@@ -57,6 +57,16 @@ function numberToChoice(number) {
      document.getElementById('rock').remove();
      document.getElementById('paper').remove();
      document.getElementById('scissors').remove();
+
+     var humanDiv = document.createElement('div');
+     var botDiv = document.createElement('div');
+     var messageDiv = document.createElement('div');
+
+     humanDiv.innerHTML = "<img src='" + ImagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50 px rgba(37, 50, 233, 1);'>"
+     botDiv.innerHTML = "<img src='" + ImagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50 px rgba(243, 38, 24, 1);'>"
+
+     document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+
  }
 
 
